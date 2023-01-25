a1b36ab09f1ecaa63b167c26348325f1ce236ea0
Alasdair Greenland <60007754+alasdair-greenland@users.noreply.github.com>
2023-01-25 02:20:09 +0000

    getTileArray function working

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ ins.hello = function() {
 }
 
 ins.getTileArray = function() {
-  let grid = document.getElementsByClassName("tileContainer")[0];
+  let grid = document.getElementsByClassName("tile-container")[0];
   let tiles = [];
   for (let i = 0; i < 16; i++) tiles[i] = 0;
   let tileEles = grid.children;
   for (let i = 0; i < tileEles.length; i++) {
-    let str = tileEles[i].className.split(" ");
-    let posString = str[2];
-    let x = posString.split('-')[2];
-    let y = posString.split('-')[3];
-    tiles[(y - 1) * 4 + x - 1] = str[1].split("-")[1];
+    let arr = tileEles[i].className.split(" ");
+    let posString = arr[2];
+    let x = Number(posString.split('-')[2]);
+    let y = Number(posString.split('-')[3]);
+    tiles[(y - 1) * 4 + x - 1] = Number(arr[1].split("-")[1]);
   }
   return tiles;
 }
