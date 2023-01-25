e4fe60d1faac7081d20c46150435fb245f6b0124
Alasdair Greenland <60007754+alasdair-greenland@users.noreply.github.com>
2023-01-25 02:10:29 +0000

    Implemented getTileArray function

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 alert('loaded 2048 insert!!');
 
-window.onkeypress = function(e) {
-  if (e.key == "P") {
-    console.log("p");
-  }
-}
-
 let ins = {};
 
 ins.hello = function() {
   alert('hello, world!!!!');
+}
+
+ins.getTileArray = function() {
+  let grid = document.getElementsByClassName("tileContainer")[0];
+  let tiles = [];
+  for (let i = 0; i < 16; i++) tiles[i] = 0;
+  let tileEles = grid.children;
+  for (let i = 0; i < tileEles.length; i++) {
+    let str = tileEles[i].className.split(" ");
+    let posString = str[2];
+    let x = posString.split('-')[2];
+    let y = posString.split('-')[3];
+    tiles[(y - 1) * 4 + x - 1] = str[1].split("-")[1];
+  }
+  return tiles;
 }
